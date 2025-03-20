3caf096001e8621f7c038e4579b4280e4a95fafd
YourGitHubUsername <jmwaura@byupathway.edu>
2025-03-20 10:44:33 +0300

    commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,41 +6,43 @@ class Program
     {
         Journal journal = new Journal();
         bool running = true;
-        
+
         while (running)
         {
-            Console.WriteLine("Journal Program");
+            Console.WriteLine("\nJournal Menu:");
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display the journal");
             Console.WriteLine("3. Save the journal to a file");
             Console.WriteLine("4. Load the journal from a file");
             Console.WriteLine("5. Exit");
             Console.Write("Choose an option: ");
-            
+
             string choice = Console.ReadLine();
-            
             switch (choice)
             {
                 case "1":
-                    journal.WriteEntry();
+                    journal.AddEntry();
                     break;
                 case "2":
                     journal.DisplayEntries();
                     break;
                 case "3":
-                    journal.SaveToFile();
+                    Console.Write("Enter filename to save: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveToFile(saveFile);
                     break;
                 case "4":
-                    journal.LoadFromFile();
+                    Console.Write("Enter filename to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
                     break;
                 case "5":
                     running = false;
                     break;
                 default:
-                    Console.WriteLine("Invalid choice. Please try again.");
+                    Console.WriteLine("Invalid option. Please try again.");
                     break;
             }
-            Console.WriteLine();
         }
     }
 }
